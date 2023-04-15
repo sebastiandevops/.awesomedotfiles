e2a75feba040495c84e7a1cce2ce2674ad45a576
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-04-15 10:30:04 -0500

    trash deleted

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -54,7 +54,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty --single-instance"
