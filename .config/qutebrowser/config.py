a3492775724e0c163aa550d377788c2faf41e186
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-04-17 08:25:22 -0500

    darkmode algorithm => lightness-hsl, darkmode-contrast => -0.2

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -238,8 +238,8 @@ c.colors.webpage.preferred_color_scheme = 'dark'
 # - "With selective inversion of everything": Combines the two variants
 # above.
 c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
-c.colors.webpage.darkmode.contrast = 0.0
+c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
+c.colors.webpage.darkmode.contrast = -0.02
 c.colors.webpage.darkmode.policy.images = 'never'
 
 c.url.searchengines = {
