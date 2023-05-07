1a1ecb2e70cc647f0155cecc79fa5462b300f303
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-05-07 10:36:47 -0500

    qutebrowser profiles config added

---
+++ .local/share/qutebrowser-profiles/home/config/config.py
@@ -0,0 +1,3 @@
+config.load_autoconfig()
+c.window.title_format = '{perc}{current_title}{title_sep} qutebrowser (personal)'
+config.source('/home/sebastian/.config/qutebrowser/config.py')
---
+++ .local/share/qutebrowser-profiles/pragma/config/config.py
@@ -0,0 +1,3 @@
+config.load_autoconfig()
+c.window.title_format = '{perc}{current_title}{title_sep} qutebrowser (pragma)'
+config.source('/home/sebastian/.config/qutebrowser/config.py')
