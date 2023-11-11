657126256cec20bc3a42d8e683da88670d409e39
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-11-11 17:32:55 -0500

    rofi: fonts and layout updated

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -36,9 +36,9 @@ rofi_cmd() {
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: true; width: 250px; padding: 30% 45%;}' \
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: true; width: 100px; padding: 30% 40%;}' \
 		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'listview {columns: 1; lines: 2;}' \
 		-theme-str 'element-text {horizontal-align: 0.5;}' \
 		-theme-str 'textbox {horizontal-align: 0.5;}' \
 		-dmenu \
